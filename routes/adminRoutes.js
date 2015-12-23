9b0570bf1cfd1ac1c9a1a906317ee4bfcaaa5a9b
krisdigitx <alpha@krisindigitalage.com>
2015-12-23 18:20:25 +0000

    use mongodb connectors

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,66 @@
 var express = require('express');
 var adminRouter = express.Router();
-
+var mongodb = require('mongodb').MongoClient;
+var books = [
+            {
+                title: 'War and Peace',
+                genre: 'Historical Fiction',
+                author: 'krishna shk',
+                read: false
+            },
+            {
+                title: '10AM',
+                genre: 'Science Fiction',
+                author: 'in the might',
+                read: false
+            },
+            {
+                title: '5PM',
+                genre: 'Science Fiction',
+                author: 'in the might',
+                read: false
+            },
+            {
+                title: 'Alphabet House',
+                genre: 'Science Fiction',
+                author: 'in the might',
+                read: false
+            },
+            {
+                title: 'Ginko Biloba',
+                genre: 'Science Fiction',
+                author: 'shyamalan',
+                read: false
+            },
+            {
+                title: 'MMA Muscle Pro',
+                genre: 'UFC',
+                author: 'Dana White',
+                read: false
+            },
+            {
+                title: 'twenty for 2',
+                genre: 'Thriller ',
+                author: 'speedster',
+                read: false
+            }
+    
+    ];
 var router = function(nav){
     adminRouter.route('/addBooks')
         .get(function(req,res){
-           res.send('inserting books'); 
+          var url = "mongodb://bookuser:bookpassword@ds035735.mongolab.com:35735/libraryapp";
+           mongodb.connect(url, function(err, db){
+               if (err){
+                   return console.dir(err);
+               }
+               var collection = db.collection('books');
+               collection.insertMany(books, 
+                    function(err,results){
+                           res.send(results);
+                           db.close();
+               });
+           })
         });
 
     return adminRouter;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,50 +1,49 @@
 var express = require('express');
 var bookRouter = express.Router();
-
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function(nav){
-    var books = [
-            {
-                title: 'War and Peace',
-                genre: 'Historical Fiction',
-                author: 'krishna shk',
-                read: false
-            },
-            {
-                title: '10AM',
-                genre: 'Science Fiction',
-                author: 'in the might',
-                read: false
-            },
-            {
-                title: 'twenty for 2',
-                genre: 'Thriller ',
-                author: 'speedster',
-                read: false
-            }
-    
-    ];
+
     bookRouter.route('/').get(function(req,res){
-    res.render('bookListView',{
-                    title: 'Books!', 
-                    nav: nav,
-                    books: books
-                    
+              
+    var url = "mongodb://bookuser:bookpassword@ds035735.mongolab.com:35735/libraryapp";
+    
+    mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.find({}).toArray(function (err, results){
+        res.render('bookListView',{
+                        title: 'Books!', 
+                        nav: nav,
+                        books: results
+                        
+        });  
+        
+        });
+        
     });
+
 });
 
     bookRouter.route('/:id').get(function(req,res){
-   var id = req.params.id;
-   res.render('bookView',{
-                    title: 'Books!', 
-                    nav: nav,
-                    book: books[id]
-                    
+        var id = new objectId(req.params.id);
+        var url = "mongodb://bookuser:bookpassword@ds035735.mongolab.com:35735/libraryapp";
+    
+    mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.findOne({_id: id}).toArray(function (err, results){
+        res.render('bookView',{
+                        title: 'Books!', 
+                        nav: nav,
+                        book: results
+                });  
+        
+        });
+    
     });
-   //res.send("Hello single book!"); 
-});
+    
     return bookRouter;
-}
+};
 
 
 module.exports = router;
